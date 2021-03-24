b08050658f088cd4afb6e62fc81659c9e7c7d9e3
artash_mardoyan <artashmardoyan@gmail.com>
2021-03-24 15:12:16 +0400

    Wrote the basic logic

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# koa-pagination
+# koa-pagination-v2
+
+## Installation
+
+Install the package via `yarn`:
+
+```sh
+> yarn add koa-pagination-v2
+```
+
+or via `npm`:
+
+```sh
+> npm install koa-pagination-v2 --save
+```
--- src/index.js
+++ src/index.js
@@ -1,16 +1,18 @@
-const _ = require('lodash');
+module.exports = ({ maxLimit, defaultLimit }) => {
+    const _maxLimit = typeof maxLimit === 'number' ? parseInt(maxLimit, 10) : 100;
+    const _defaultLimit = typeof defaultLimit === 'number' ? parseInt(defaultLimit, 10) : 10;
 
-module.exports = () => {
     return async (ctx, next) => {
-        const [min, max, def] = [2, 100, 10];
-        let { limit, offset, page } = ctx.query;
+        let { page, limit } = ctx.query;
 
-        const pageValue = _.toNumber(page >= 1 ? page : 1);
+        page = Math.abs(parseInt(page, 10)) || 1;
+        limit = Math.abs(parseInt(limit, 10)) || _defaultLimit;
 
-        limit = _.toNumber(limit >= min && limit <= max ? limit : def);
-        offset = page ? (pageValue - 1) * limit : _.toNumber(Math.max(0, offset)) || 0;
+        limit = limit > _maxLimit ? _maxLimit : limit;
 
-        ctx.state.paginate = { limit, offset, page: pageValue };
+        const offset = page * limit - limit;
+
+        ctx.state.paginate = { limit, page, offset, skip: offset };
 
         await next();
     };
