b87c054139219492cab4a0ff2480c9262ac62da5
artash_mardoyan <artashmardoyan@gmail.com>
2021-03-24 15:52:42 +0400

    Update function parameters

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-module.exports = ({ maxLimit, defaultLimit }) => {
-    const _maxLimit = typeof maxLimit === 'number' ? parseInt(maxLimit, 10) : 100;
+module.exports = ({ maximumLimit, defaultLimit }) => {
+    const _maximumLimit = typeof maximumLimit === 'number' ? parseInt(maximumLimit, 10) : 100;
     const _defaultLimit = typeof defaultLimit === 'number' ? parseInt(defaultLimit, 10) : 10;
 
     return async (ctx, next) => {
@@ -8,7 +8,7 @@ module.exports = ({ maxLimit, defaultLimit }) => {
         page = Math.abs(parseInt(page, 10)) || 1;
         limit = Math.abs(parseInt(limit, 10)) || _defaultLimit;
 
-        limit = limit > _maxLimit ? _maxLimit : limit;
+        limit = limit > _maximumLimit ? _maximumLimit : limit;
 
         const offset = page * limit - limit;
 
