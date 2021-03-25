f37520584866ce59513ac21b5818b7ec1e7ff1ca
artash_mardoyan <artashmardoyan@gmail.com>
2021-03-25 17:16:39 +0400

    Added simple test schemas

--- __tests__/index.js
+++ __tests__/index.js
@@ -1,5 +1,26 @@
-describe('middleware', () => {
-    test('two plus two is four', () => {
-        expect(2 + 2).toBe(4);
+const { createMockContext } = require('@shopify/jest-koa-mocks');
+const pagination = require('..');
+
+describe('middleware being tested', () => {
+    let ctx;
+    let next;
+    let middleware;
+
+    beforeEach(() => {
+        next = jest.fn();
+        ctx = createMockContext();
+        middleware = pagination();
+    });
+
+    test('ctx.state should to have property paginate', async () => {
+        await middleware(ctx, next);
+
+        expect(ctx.state).toHaveProperty('paginate');
+    });
+
+    test('ctx.state.pagination should to match object', async () => {
+        await middleware(ctx, next);
+
+        expect(ctx.state.paginate).toMatchObject({ limit: 10, page: 1, offset: 0, skip: 0 });
     });
 });
